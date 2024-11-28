cd124ef78a8be913b33af0f164b833c6efdad0ba
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-27 21:03:20 -0500

    Refactor layout and lists screens with enhanced UI components and scrolling functionality

--- lib/screens/layout_screen.dart
+++ lib/screens/layout_screen.dart
@@ -12,8 +12,8 @@ class LayoutScreen extends StatelessWidget {
         title: const Text("Layout"),
         // Widget custom
         leading: IconButton(
-          onPressed: (){
-            /// Navegación hacia atrás 
+          onPressed: () {
+            /// Navegación hacia atrás
             /// push -> ir hacia adelante
             /// pop -> ir hacia atrás
             Navigator.pop(context);
@@ -21,11 +21,106 @@ class LayoutScreen extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
+
       /// 1. Column
       /// 2. Row
       /// 3. Container
       body: Column(
-        children: [],
+        // El column tiene propiedades
+        // Alinear los elementos en el eje principal -> mainAxisAlignment
+        // Alinear los elementos en el eje secundario -> crossAxisAlignment
+        // Definir el tanaño del column verticalmente -> mainAxisSize
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          /// Widgets
+          /// Container -> Caja, color, modificar los bordes, sombreados, tamaños, etc.
+          Container(
+            color: Colors.blue,
+            height: 100,
+            // Revisar
+            // width: (context.size?.width ?? 0) / 2,
+            // 1. MediaQuery.of(context).size.width -> Tamaño de la pantalla
+            // 2. LayoutBuilder -> Nos permite obtener el tamaño de la pantalla -> https://api.flutter.dev/flutter/widgets/LayoutBuilder-class.html
+            width: MediaQuery.of(context).size.width / 2,
+          ),
+
+          // SizedBox -> Espacio -> caja vacía con un tamaño definido
+          const SizedBox(
+            height: 20,
+          ),
+
+          Container(
+            color: Colors.yellow,
+            width: 100,
+            height: 100,
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          Container(
+            color: Colors.green,
+            width: 50,
+            height: 50,
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          /// Row -> Filas -> Colocar widgets de manera horizontal
+          /// mainAxisAlignment -> eje horizontal
+          /// crossAxisAlignment -> eje vertical
+          /// mainAxisSize -> tamaño de la fila
+          Row(
+            children: [
+              Container(
+                color: Colors.yellow,
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10.0),
+                  gradient: const LinearGradient(
+                    stops: [0.1, 0.2, 1.0],
+                    colors: [
+                      Colors.yellow,
+                      Colors.red,
+                      Colors.blue,
+                    ],
+                  ),
+                ),
+                width: 50,
+                height: 50,
+              ),
+              Tooltip(
+                message: "HOLA",
+                child: Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+
+              // GestureDetector -> Detectar gestos -> envolver el widget que quieres que tenga un gesto
+              GestureDetector(
+                onTap: () {
+                  print("Hola mundo");
+                },
+                child: Container(
+                  color: Colors.orange,
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/screens/lists_screen.dart
+++ lib/screens/lists_screen.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
-class ListsScreen extends StatelessWidget {
+class ListsScreen extends StatefulWidget {
   const ListsScreen({super.key});
 
   static const routeName = "/lists";
 
   @override
+  State<ListsScreen> createState() => _ListsScreenState();
+}
+
+class _ListsScreenState extends State<ListsScreen> {
+  final listDeElementos = List.generate(10000, (index) => index);
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Listas"),
+      ),
+
+      /// Expanded -> Widget todo lo que tiene disponible
+      body: Row(
+        children: [
+          /// 1. ListView
+          Expanded(
+            child: ListView(
+              // Dirección del scroll
+              // scrollDirection: Axis.vertical,
+              // Física del scroll
+              // BouncingScrollPhysics -> fisica iOS
+              // AlwaysScrollableScrollPhysics -> fisica Android
+              // NeverScrollableScrollPhysics-> No se puede hacer scroll
+              // physics: AlwaysScrollableScrollPhysics(),
+              children: const [
+                Text("Elemento 1"),
+                Text("Elemento 2"),
+                Text("Elemento 3"),
+                Text("Elemento 4"),
+                Text("Elemento 5"),
+                Text("Elemento 6"),
+                Text("Elemento 7"),
+                Text("Elemento 8"),
+              ],
+            ),
+          ),
+
+          /// 2. SingleChildScrollView -> acompañado del column - row
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                /// ESTO NO ES OPTIMO!!!!
+                // children: listDeElementos.map((element) {
+                //   /// [0,1,2,3,4,5,6]
+                //   /// map -> element = 0
+                //   /// map -> element = 1
+                //   return Text("Elemento $element");
+                // }).toList(),
+                children: [
+                  Container(
+                    color: Colors.red,
+                    height: 500,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 500,
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          /// 3. ListView.builder
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (_, index) {
+                /// 0 -> listaDeElementos[0]
+                /// 1 -> listaDeElementos[1]
+                /// 2 -> listaDeElementos[2]
+                return Text("Elemento es ${listDeElementos[index]}");
+              },
+              itemCount: listDeElementos.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
