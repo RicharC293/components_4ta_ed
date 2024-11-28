5254bd365d71114808f3be819a65a7c1fca0faa6
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-27 19:06:40 -0500

    Add new screen routes for buttons, alerts, forms, infinity scroll, and animations

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,9 +1,31 @@
+import 'package:components/screens/alerts_screen.dart';
+import 'package:components/screens/animations_screen.dart';
+import 'package:components/screens/buttons_screen.dart';
+import 'package:components/screens/forms_screen.dart';
 import 'package:components/screens/home_screen.dart';
+import 'package:components/screens/infinity_scroll_screen.dart';
 import 'package:components/screens/layout_screen.dart';
+import 'package:components/screens/lists_screen.dart';
 
 final routes = {
   /// Clave y Valor
   /// El nombre de la pantalla : builder -> (context) => NombrePantalla(),
   HomeScreen.routeName: (context) => const HomeScreen(),
-  LayoutScreen.routeName : (context) => const LayoutScreen(),
+  LayoutScreen.routeName: (context) => const LayoutScreen(),
+  ListsScreen.routeName: (context) => const ListsScreen(),
+
+  /// buttons_screen.dart
+  ButtonsScreen.routeName: (context) => const ButtonsScreen(),
+
+  /// alerts_screen.dart
+  AlertsScreen.routeName: (context) => const AlertsScreen(),
+
+  /// forms_screen.dart
+  FormsScreen.routeName: (context) => const FormsScreen(),
+
+  /// infinity_scroll_screen.dart
+  InfinityScrollScreen.routeName: (context) => const InfinityScrollScreen(),
+
+  /// animations_screen.dart
+  AnimationsScreen.routeName: (context) => const AnimationsScreen(),
 };
