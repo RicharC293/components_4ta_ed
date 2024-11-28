6cfbeddfa225fb06a064759bb0dc356016b66a53
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-27 21:19:41 -0500

    Update ButtonsScreen with new route name and implement custom UI layout

--- lib/screens/buttons_screen.dart
+++ lib/screens/buttons_screen.dart
@@ -3,10 +3,29 @@ import 'package:flutter/material.dart';
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
 
-  static const routeName = "/buttons";
+  static const routeName = "/buttons-screen";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: const Text("Buttoms Scren"),
+            floating: true,
+            pinned: true,
+            expandedHeight: 180.0,
+            flexibleSpace: Image.network(
+              'https://cms-imgp.jw-cdn.org/img/p/2023605/univ/art/2023605_univ_lsr_lg.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          //SliverList.builder(itemBuilder: (context, index) {
+          //  return const Text("Hola");
+          //})
+          SliverList.list(children: const [Text("Hola")])
+        ],
+      ),
+    );
   }
 }
