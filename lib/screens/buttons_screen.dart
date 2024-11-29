4d17ade364261f6a02bdbff8c8642756b6181435
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-28 19:42:02 -0500

    Refactor ButtonsScreen to use CustomScrollView with Slivers and add FilledButton

--- lib/screens/buttons_screen.dart
+++ lib/screens/buttons_screen.dart
@@ -8,22 +8,71 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /// CustomScrollView -> Slivers (SliverAppBar, SliverList, SliverGrid)
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            title: const Text("Buttoms Scren"),
+            title: const Text("Buttons Screen"),
             floating: true,
             pinned: true,
-            expandedHeight: 180.0,
+            expandedHeight: 150.0,
             flexibleSpace: Image.network(
-              'https://cms-imgp.jw-cdn.org/img/p/2023605/univ/art/2023605_univ_lsr_lg.jpg',
+              'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
               fit: BoxFit.cover,
             ),
           ),
           //SliverList.builder(itemBuilder: (context, index) {
           //  return const Text("Hola");
           //})
-          SliverList.list(children: const [Text("Hola")])
+          // SliverList.list -> ListView (Widget)
+          // SliverList.builder -> ListView.builder (Widget)
+          // SliverList.separated -> ListView.separated (Widget) -> Separador
+          SliverList.list(
+            children: [
+              /// 1. FilledButton
+              const Text("Filled Button"),
+              FilledButton(
+                /// onPressed -> Función que se ejecuta al presionar el botón
+                /// (){} -> Función vacía -> No hace nada -> habilitado
+                /// null -> Botón deshabilitado
+                onPressed: () {
+                  print("Filled Button");
+                  // Llamar un api
+                  // Navegar a otra pantalla
+                },
+
+                /// ButtonStyle -> documentación oficial -> https://api.flutter.dev/flutter/material/ButtonStyle-class.html
+                style: ButtonStyle(
+                  /// cambia el color del botón
+                  /// WidgetStateProperty<Color> -> Color
+                  /// WidgetStateProperty.all(Colors.amber) -> todo a amarillo
+                  backgroundColor: WidgetStateProperty.all(Colors.amber),
+                  // backgroundColor: WidgetStateProperty.resolveWith(
+                  //   (states) {
+                  //     if (states.contains(WidgetState.pressed)) {
+                  //       return Colors.red;
+                  //     }
+                  //     return Colors.amber;
+                  //   },
+                  // ),
+                ),
+
+                /// child -> Widget dentro de ese botón
+                /// CircularProgressIndicator -> Widget de carga
+                // child: Center(
+                //   child: CircularProgressIndicator(
+                //     color: Colors.white,
+                //   ),
+                // ),
+                child: const Text("FilledButton"),
+              ),
+
+              /// 2. OutlinedButton
+              /// 3. TextButton
+              /// 4. ElevatedButton
+              /// 5. IconButton
+            ],
+          ),
         ],
       ),
     );
