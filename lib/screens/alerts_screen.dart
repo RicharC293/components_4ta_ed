a55864c9935f50c8b9b11101cb77627bdbd896c7
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-28 20:48:12 -0500

    Implement AlertsScreen with dialog functionality and platform-specific alert buttons

--- lib/screens/alerts_screen.dart
+++ lib/screens/alerts_screen.dart
@@ -7,6 +7,56 @@ class AlertsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alerts"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ///https://api.flutter.dev/flutter/material/showDialog.html
+            TextButton(
+              onPressed: () {
+                /// 1. Usar el showDialog
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    /// 2. Retornar un AlertDialog
+                    return AlertDialog(
+                      title: Text("Alerta Android"),
+                      content: Text("Este es un mensaje de alerta en Android"),
+                      actions: [
+                        /// 3. Agregar los botones
+                        /// Botón cancelar -> OutinedButton
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: const Text("Cancelar"),
+                        ),
+
+                        /// Botón aceptar -> FilledButton
+                        FilledButton(
+                          onPressed: () {},
+                          child: const Text("Aceptar"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text("Alerta Android"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Alerta iOS"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Alerta por plataforma"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
