4cc7213a3a40abd305437e2ab59270d1631677b4
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-12-02 19:50:05 -0500

    Add asset paths to pubspec.yaml and implement FormsScreen with image rendering

--- lib/screens/forms_screen.dart
+++ lib/screens/forms_screen.dart
@@ -7,6 +7,23 @@ class FormsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Formularios e Imagenes"),
+      ),
+      body: Column(
+        children: [
+          /// Crear Imagenes - Renderizar Imagenes -> Image (Proveedor) - Widget
+          /// 1. A partir de los assets 
+          /// (AssetImage) (Proveedor) -> Image (Widget) -> Ciertos widgets te piden un provider
+          Image(
+            image: AssetImage("assets/images/image.jpeg"),
+          ),
+          
+          /// 2. A partir de un enlace de internet -> .jpg, .png, .gif .webp(Funciona?)
+          /// 3. A partir de un base64
+        ],
+      ),
+    );
   }
 }
